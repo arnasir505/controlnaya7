298b094464c945adb0736c94f122377cefb688c0
Ariet Nasirdinov <ariet.nasirdinov505@gmail.com>
2024-01-13 14:29:08 +0600

    Created order list

--- src/App.tsx
+++ src/App.tsx
@@ -4,10 +4,49 @@ import { Menu } from './menuData';
 import './components/ItemButton/ItemButton.css';
 
 function App() {
-  const [currentOrder, setCurrentOrder] = useState([]);
+  const [currentOrder, setCurrentOrder] = useState([
+    {
+      id: 1,
+      name: 'Hamburger',
+      count: 0,
+    },
+    {
+      id: 2,
+      name: 'Hotdog',
+      count: 0,
+    },
+    {
+      id: 3,
+      name: 'Burrito',
+      count: 0,
+    },
+    {
+      id: 4,
+      name: 'Fries',
+      count: 0,
+    },
+    {
+      id: 5,
+      name: 'Tea',
+      count: 0,
+    },
+    {
+      id: 6,
+      name: 'Cola',
+      count: 0,
+    },
+  ]);
 
-  const addToOrder = () => {
-    console.log('click');
+  const addToOrder = (id: number) => {
+    setCurrentOrder((prevState) => {
+      return prevState.map((order) => {
+        if (order.id === id) {
+          return { ...order, count: order.count + 1 };
+        } else {
+          return order;
+        }
+      });
+    });
   };
 
   const menuItems = Menu.map((item) => {
@@ -15,7 +54,7 @@ function App() {
       <button
         className='btn menuBtn d-flex gap-2'
         key={item.id}
-        onClick={addToOrder}
+        onClick={() => addToOrder(item.id)}
       >
         <img src={item.img} alt={item.name} className='menuBtn_img' />
         <div className='btn-text d-flex flex-column align-items-baseline'>
@@ -26,21 +65,24 @@ function App() {
     );
   });
 
-  // const orderItems = currentOrder.map((item) => {
-  //   return (
-  //     <div>
-  //       <span>{item.name}</span>
-  //       <span>{item.count}</span>
-  //     </div>
-  //   );
-  // });
+  const orderItems = currentOrder
+    .filter((order) => order.count > 0)
+    .map((order) => {
+      return (
+        <div key={order.id}>
+          {order.name} {order.count}
+        </div>
+      );
+    });
   return (
     <div className='container d-flex mt-5'>
       <div className='menuWrap d-flex w-50 flex-wrap justify-content-between gap-2'>
         {menuItems}
       </div>
       <div className='orderWrap'>
-        {/* {orderItems.length > 0 ? orderItems : 'Order is empty'} */}
+        {currentOrder.filter((order) => order.count > 0).length > 0
+          ? orderItems
+          : 'Empty'}
       </div>
     </div>
   );
